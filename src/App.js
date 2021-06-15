857e081428a47ec0cd672bac8c7fee149fdfe5e3
FREEZ2385 <schlee9703@naver.com>
2021-06-15 23:27:22 +0900

    [Initial] Test of garland api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
+import "./App.css";
+
+async function itemCall(item){
+  axios.get(`https://www.garlandtools.org/db/doc/item/ja/3/${item}.json`).then(
+    (response) => response.data)
+}
+
+async function equipCall(){
+  const data = axios.get(`https://www.garlandtools.org/db/doc/equip/ja/2/leveling-drk.json`
+  ).then((response) => response.data)
+  return data;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [lists, setLists] = useState([]);
+
+  useEffect(()=>{
+    equipCall().then((response) => {
+      console.log(response);
+      const helmatList = response.equip.map((data) => {
+        console.log(data)
+        const itemName = itemCall(data[3][0].id);
+        console.log(itemName)
+        return itemName.item.name;
+      });
+      setLists(helmatList);
+    });;
+  }, [])
+
+  console.log(lists)
+  
+
+  return (<div className="App">
+    <button>test</button>
+    <div>{ lists.map((t,k) => <div><text key={k}>{t} </text></div>) }</div>
+  </div>);
 }
 
 export default App;
